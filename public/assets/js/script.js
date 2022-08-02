62a67c372c2167939ba996bd14010156428406d4
Alexis Ferrer <alexisferrer60160@gmail.com>
2022-08-02 14:45:23 +0200

    suppresion des console.log

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -9,8 +9,6 @@ fetch("public/assets/js/movies.json")
         let description = element.overview;
         let poster = element.poster_path;
         let note = Math.floor(element.vote_average/2);
-        console.log(note);
-        console.log(typeof note);
         document.querySelector('.container').innerHTML += 
         `<div class="card">
             <div class="moviesPoster">
@@ -32,45 +30,24 @@ fetch("public/assets/js/movies.json")
                 </div>
             </div>
         </div>`
-        
-        // for (let i = 0; i < note; i++) {
-            
-        // }
-
-        // if (note == 0) {
-        //     document.querySelector("#star1").classList.add('greyStars');
-        //     document.querySelector("#star2").classList.add('greyStars');
-        //     document.querySelector("#star3").classList.add('greyStars');
-        //     document.querySelector("#star4").classList.add('greyStars');
-        //     document.querySelector("#star5").classList.add('greyStars');
-        // }
         if (note == 1) {
             document.querySelectorAll(".star1")[count].classList.add('yellowStars');
-            // document.querySelector("#star2").classList.add('greyStars');
-            // document.querySelector("#star3").classList.add('greyStars');
-            // document.querySelector("#star4").classList.add('greyStars');
-            // document.querySelector("#star5").classList.add('greyStars');
         }
         else if (note == 2) {
             document.querySelectorAll(".star1")[count].classList.add('yellowStars');
             document.querySelectorAll(".star2")[count].classList.add('yellowStars');
-            // document.querySelector("#star3").classList.add('greyStars');
-            // document.querySelector("#star4").classList.add('greyStars');
-            // document.querySelector("#star5").classList.add('greyStars');
         }
         else if (note == 3) {
             document.querySelectorAll(".star1")[count].classList.add('yellowStars');
             document.querySelectorAll(".star2")[count].classList.add('yellowStars');
             document.querySelectorAll(".star3")[count].classList.add('yellowStars');
-            // document.querySelector("#star4").classList.add('greyStars');
-            // document.querySelector("#star5").classList.add('greyStars');
         }
         else if (note == 4) {    
             document.querySelectorAll(".star1")[count].classList.add('yellowStars');
             document.querySelectorAll(".star2")[count].classList.add('yellowStars');
             document.querySelectorAll(".star3")[count].classList.add('yellowStars');
             document.querySelectorAll(".star4")[count].classList.add('yellowStars');
-            // document.querySelector("#star5").classList.add('greyStars');
+
         }
         else if (note == 5) {
             document.querySelectorAll(".star1")[count].classList.add('yellowStars');
